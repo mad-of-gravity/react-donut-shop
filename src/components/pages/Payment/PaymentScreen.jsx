cae718ccb5b18c651d54b56b75f445302e89c607
mad-of-gravity <mad.of.gravity@gmail.com>
2023-02-04 20:12:19 +0200

    adding donuts to the footer of the page

--- src/components/pages/Payment/PaymentScreen.jsx
+++ src/components/pages/Payment/PaymentScreen.jsx
@@ -1,69 +1,98 @@
 import "./styles.css";
 import Title from "../../Title/Title";
+import Donut from "../../DonutComponent/Donut";
 import { useEffect } from "react";
 
 const PaymentScreen = () => {
+  useEffect(() => {
+    document.body.style.backgroundColor = "#FF86B3";
+  }, []);
 
-    useEffect(() => {
-        document.body.style.backgroundColor = "#FF86B3";
-    }, []);
+  const titleStyle = {
+    small: {
+      margin: "-15px 0",
+      padding: "0",
+      fontFamily: "'Outfit', sans-serif",
+      fontWeight: "900",
+      fontSize: "48px",
+      lineHeight: "72px",
+      alignSelf: "flex-end",
+      color: "#FFFFFF",
+    },
 
-    const titleStyle = {
-        small: {
-            margin: "-15px 0",
-            padding: "0",
-            fontFamily: "'Outfit', sans-serif",
-            fontWeight: "900",
-            fontSize: "48px",
-            lineHeight: "72px",
-            alignSelf: "flex-end",
-            color: "#FFFFFF",
-        },
+    middle: {
+      margin: "-15px 0",
+      padding: "0",
+      fontFamily: "'Outfit', sans-serif",
+      fontWeight: "900",
+      fontSize: "64px",
+      lineHeight: "72px",
+      alignSelf: "flex-end",
+      color: "#FFFFFF",
+    },
 
-        middle: {
-            margin: "-15px 0",
-            padding: "0",
-            fontFamily: "'Outfit', sans-serif",
-            fontWeight: "900",
-            fontSize: "64px",
-            lineHeight: "72px",
-            alignSelf: "flex-end",
-            color: "#FFFFFF",
-        },
-    
-        large: {
-            margin: "0",
-            padding: "0",
-            fontFamily: "'Outfit', sans-serif",
-            fontWeight: "900",
-            fontSize: "96px",
-            lineHeight: "72px",
-            textAlign: "end",
-            color: "#FFFFFF",
-        }
-     }
+    large: {
+      margin: "0",
+      padding: "0",
+      fontFamily: "'Outfit', sans-serif",
+      fontWeight: "900",
+      fontSize: "96px",
+      lineHeight: "72px",
+      textAlign: "end",
+      color: "#FFFFFF",
+    },
+  };
 
-     const handlePaymentButtonClick = (selectedButton) => {
-        console.log(selectedButton + " button has been clicked!");
-     }
+  const handlePaymentButtonClick = (selectedButton) => {
+    console.log(selectedButton + " button has been clicked!");
+  };
 
-    return (
-        <div className="container">
-            <div className="title">
-                <Title sx={titleStyle.middle}>You can</Title>
-                <Title sx={titleStyle.small}>PAY US</Title>
-                <Title sx={titleStyle.large}>NOW</Title>
-            </div>
-
-            <button className="pay-button" onClick={() => handlePaymentButtonClick("Apple")}>
-                <img src="assets/apple-pay.png" />
-            </button>
-
-            <button className="pay-button" onClick={() => handlePaymentButtonClick("Google")}>
-                <img src="assets/google-pay.png" />
-            </button>
+  return (
+    <>
+      <div className="container">
+        <div className="title">
+          <Title sx={titleStyle.middle}>You can</Title>
+          <Title sx={titleStyle.small}>PAY US</Title>
+          <Title sx={titleStyle.large}>NOW</Title>
         </div>
-    );
-}
+
+        <button
+          className="pay-button"
+          onClick={() => handlePaymentButtonClick("Apple")}
+        >
+          <img src="assets/apple-pay.png" />
+        </button>
+
+        <button
+          className="pay-button"
+          onClick={() => handlePaymentButtonClick("Google")}
+        >
+          <img src="assets/google-pay.png" />
+        </button>
+      </div>
+
+      <div className="footer">
+        <div className="sky-shaped">
+          <Donut
+            className="sky-shaped"
+            variant="static"
+            donutSrc="assets/sky_shaped.svg"
+            width="auto"
+            height="auto"
+          />
+        </div>
+        <div className="unicorn-dust">
+          <Donut
+            className="unicorn-dust"
+            variant="static"
+            donutSrc="assets/unicorn_dust.svg"
+            width="auto"
+            height="auto"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default PaymentScreen;
