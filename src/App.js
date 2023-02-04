bc1a1ebc9bb462b6b88e76c63045fdf1dd3d6ea7
mad-of-gravity <mad.of.gravity@gmail.com>
2023-02-04 15:16:46 +0200

    Install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import "./App.css";
+import StartScreen from "./components/pages/start/StartScreen";
 
 function App() {
   return (
-    <div className="App">
-      <section class="hero">
-        <div class="hero-body">
-          <p class="title">A React Task</p>
-          <p class="subtitle">by Boom.dev</p>
-        </div>
-      </section>
-      <div class="container is-fullhd">
-        <div class="notification">
-          Edit the <code>./src</code> folder to add components.
-        </div>
-      </div>
-    </div>
+    <StartScreen />
   );
 }
 
