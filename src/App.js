332f4716b8a744afee9f0f9427ec754c8776ac19
mad-of-gravity <mad.of.gravity@gmail.com>
2023-02-04 20:40:29 +0200

    determining the routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MenuScreen from "./components/pages/Menu/MenuScreen";
 import PaymentScreen from "./components/pages/Payment/PaymentScreen";
 
 function App() {
   return (
-    <PaymentScreen />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<MenuScreen/>} />
+        <Route path="/payment" element={<PaymentScreen/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
