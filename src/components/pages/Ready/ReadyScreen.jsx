7805d6aefc5bde920de40285a8cccf87b910e491
mad-of-gravity <mad.of.gravity@gmail.com>
2023-02-05 02:30:12 +0200

    fix some bugs

--- src/components/pages/Ready/ReadyScreen.jsx
+++ src/components/pages/Ready/ReadyScreen.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Donut from "../../DonutComponent/Donut";
 import Title from "../../TitleComponent/Title";
 import "./styles.css";
